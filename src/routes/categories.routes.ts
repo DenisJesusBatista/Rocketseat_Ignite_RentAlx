b557d0f7499baa5cbf83ae72e8e93f43e1755e9b
Denis Batista <denis.si2011@gmail.com>
2022-02-15 19:18:11 -0300

    FIX: Adicionando UUID.

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,13 +1,20 @@
 import { Router } from 'express';
+import { v4 as uuidV4 } from 'uuid';
 
 const categoriesRoutes = Router();
 
 const categories = [];
 
-categoriesRoutes.post('/categories', (request, response) => {
-    const { name, descrption } = request.body;
+categoriesRoutes.post('/', (request, response) => {
+    const { name, description } = request.body;
 
-    categories.push({ name, descrption });
+    const category = {
+        name,
+        description,
+        id: uuidV4(),
+    };
+
+    categories.push(category);
 
     return response.status(201).send();
 });
