589195e2173f702a1110b50315512a49ccfa5e51
Denis Batista <denis.si2011@gmail.com>
2022-02-18 19:12:22 -0300

    FIX: Listando as categorias.

--- src/model/Category.ts
+++ src/model/Category.ts
@@ -2,13 +2,13 @@ import { v4 as uuidv4 } from 'uuid';
 
 class Category {
     id?: string;
-    name: string;
-    description: string;
-    created_at: Date;
+    name?: string;
+    description?: string;
+    created_at?: Date;
 
     constructor() {
         if (!this.id) {
-            this.id == uuidv4();
+            this.id = uuidv4();
         }
     }
 }
